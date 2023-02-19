548eb645be4e35c3cfb314e73ea805e6216982f6
Daya Darsan <dayadarsan1802@gmail.com>
2023-02-19 22:11:47 +0530

    Add window creation

--- source/ScreenSaver.cpp
+++ source/ScreenSaver.cpp
@@ -1,4 +1,22 @@
 ﻿int main()
 {
+	glfwSetErrorCallback ( glfwErrorCallback );
+	glfwInit ();
+	
+	GLFWwindow* window = glfwCreateWindow ( 1000, 800, "ScreenSaver", nullptr, nullptr );
+	
+	glfwMakeContextCurrent ( window );
+	gladLoadGL ();
+	
+	while ( ! glfwWindowShouldClose ( window ) )
+	{
+		glClearColor ( 0, 0, 0, 1 );
+		glClear ( GL_COLOR_BUFFER_BIT );
+
+		glfwSwapBuffers ( window );
+		glfwPollEvents ();
+	}
+
 	return 0;
 }
+
