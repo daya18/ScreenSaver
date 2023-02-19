6b353a0c33dcb2554ae25714baa8a674930f71cd
Daya Darsan <dayadarsan1802@gmail.com>
2023-02-19 22:53:18 +0530

    Add OpenGL Error checking and display

--- source/ScreenSaver.cpp
+++ source/ScreenSaver.cpp
@@ -1,13 +1,43 @@
-﻿int main()
+﻿#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void debugMessageCallback ( GLenum source,
+	GLenum type,
+	GLuint id,
+	GLenum severity,
+	GLsizei length,
+	const GLchar* message,
+	const void* userParam )
+{
+	std::string type_string =
+		type == GL_DEBUG_TYPE_ERROR ? "Error"
+		: type == GL_DEBUG_TYPE_PERFORMANCE ? "Performance"
+		: type == GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR ? "Undefined Behaviour" : "Unknown type";
+
+	std::cerr << "OpenGL " << type_string << ": " << message << std::endl;
+}
+
+void glfwErrorCallback ( int code, const char* description )
+{
+	std::cout << "GLFW Error " << code << ": " << description << std::endl;
+}
+
+int main()
 {
 	glfwSetErrorCallback ( glfwErrorCallback );
 	glfwInit ();
 	
+	glfwWindowHint ( GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE );
 	GLFWwindow* window = glfwCreateWindow ( 1000, 800, "ScreenSaver", nullptr, nullptr );
 	
 	glfwMakeContextCurrent ( window );
 	gladLoadGL ();
 	
+	glEnable ( GL_DEBUG_OUTPUT );
+	glDebugMessageCallback ( debugMessageCallback, nullptr );
+
 	while ( ! glfwWindowShouldClose ( window ) )
 	{
 		glClearColor ( 0, 0, 0, 1 );
